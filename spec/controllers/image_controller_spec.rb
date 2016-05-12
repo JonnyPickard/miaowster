cee0a51a3f6bdd73ee6dd0fd89194a67a3624473
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-12 16:13:52 +0100

    Add GET #new spec for ImagesController

--- spec/controllers/image_controller_spec.rb
+++ spec/controllers/image_controller_spec.rb
@@ -2,15 +2,35 @@ require 'rails_helper'
 
 describe ImagesController do
   describe "GET #index" do
-    login_user
 
     context "when user is logged in" do
-      it "renders the index page correctly" do
+      login_user
+
+      it "renders the index page successfully" do
         get :index
 
         expect(response).to be_success
         expect(response).to render_template(:"images/index")
       end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to /users/sign_in" do
+        get :index
+
+        expect(response).to redirect_to("http://test.host/users/sign_in")
+      end
+    end
+  end
+
+  describe "GET #new" do
+    login_user
+
+    it "renders the new image page successfully" do
+      get :new
+
+      expect(response).to be_success
+      expect(response).to render_template(:"images/new")
     end
   end
 end

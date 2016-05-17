fb4c69fd39f98c8d6e3e3b950ad2677153dc25c4
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-17 16:23:50 +0100

    Refactored Endorsment model spec with correct shoulda matcher syntax

--- spec/models/endorsement/image_id_spec.rb
+++ spec/models/endorsement/image_id_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 describe Endorsement, ".user_id", type: :model do
-  it "validates the uniqueness of a user id" do
-    @endorsement = create(:endorsement)
-
-    subject { @endorsement }
-
-    expect(subject).to validate_uniqueness_of(:user_id)
-  end
+  it { is_expected.to validate_uniqueness_of(:user_id) }
 end
--- spec/models/endorsement/user_id_spec.rb
+++ spec/models/endorsement/user_id_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 describe Endorsement, ".image_id", type: :model do
-  it "validates the uniqueness of a user id" do
-    @endorsement = create(:endorsement)
-
-    subject { @endorsement }
-
-    expect(subject).to validates_presence_of(:image_id)
-  end
+  it { is_expected.to validate_presence_of(:image_id) }
 end

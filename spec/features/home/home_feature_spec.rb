a2f07af8e23de1b8dd2836ce2f6d28fb24550483
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-17 13:56:48 +0100

    Updated home feature spec

--- spec/features/home/home_feature_spec.rb
+++ spec/features/home/home_feature_spec.rb
@@ -9,4 +9,14 @@ feature "User can view home page" do
     expect(current_path).to eq(home_index_path)
     expect(page).to have_content("No pictures have been uploaded yet!")
   end
+
+  scenario "Pictures have been uploaded" do
+    sign_in
+    create_image
+
+    click_link "Trending"
+
+    expect(current_path).to eq(home_index_path)
+    expect(page).to have_content("Test")
+  end
 end

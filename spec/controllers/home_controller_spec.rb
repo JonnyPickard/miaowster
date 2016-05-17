b4acaa659613d728f57e90cb9e8632ef421923e8
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-17 13:52:57 +0100

    Add GET #index spec for HomeController

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe HomeController, type: :controller do
+describe HomeController do
+  describe "GET #index" do
+    login_user
 
+    it "renders the index page successfully" do
+      get :index
+
+      expect(response).to be_success
+      expect(response).to render_template(:"home/index")
+    end
+
+  end
 end

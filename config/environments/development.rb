1037379dee3e9ea529d87274d7d149ee470062a5
Jonny Pickard <jonny@Jonnys-MacBook-Pro.local>
2016-11-07 15:54:16 +0000

    Migrated project to new mac

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('MIAOWSTER_DEV_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket: 'miaowster-dev',
+      access_key_id: 'AKIAJX4FWKI3VIAIT47Q',
+      secret_access_key: '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',
+      s3_region: 'OREGON',
     }
   }
 
